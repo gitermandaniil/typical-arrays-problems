11f9df2049f8b7c94bd771aecd2beacda5d585d9
Daniil Giterman <giterman@corpcentre.ru>
2020-09-29 00:16:16 +0400

    feat: spread operator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const isValid = arr => arr && arr.length
 
-exports.min = arr => isValid(arr) ? Math.min.apply(null, arr) : 0
-exports.max = arr => isValid(arr) ? Math.max.apply(null, arr) : 0
+exports.min = arr => isValid(arr) ? Math.min(...arr) : 0
+exports.max = arr => isValid(arr) ? Math.max(...arr) : 0
 exports.avg = arr => isValid(arr) ? arr.reduce((a, b) => a + b) / arr.length : 0
